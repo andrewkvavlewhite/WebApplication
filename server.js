2202609d6afbef7c07ea7a5e3dac10b6072b5ab1
andrewkvavlewhite <andrewkvavlewhite@gmail.com>
2013-12-03 02:34:29 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+ 
+/* serves main page */
+app.get("/", function(req, res) {
+	res.sendfile('eCalendar.html')
+});
+
+app.get("/SUP", function(req, res) {
+	res.send({name:"Raymond",age:40});
+});
+ 
+app.post("/user/add", function(req, res) { 
+	/* some server side logic */
+	res.send("OK");
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){ 
+	console.log('static file request : ' + req.params);
+	res.sendfile( __dirname + req.params[0]); 
+});
+ 
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
