d102bbee27ccdb08178dc24e841f3dfa96bd2f4d
andrewkvavlewhite <andrewkvavlewhite@gmail.com>
2013-12-11 02:37:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 var express = require("express");
 var app = express();
- 
+var db = require('riak-js').getClient({host: "127.0.0.1", port: "10018"});
+
 /* serves main page */
 app.get("/", function(req, res) {
-	res.sendfile('eCalendar.html')
+	res.sendfile('EventCalendar.html')
+});
+
+app.get("/demo_test.txt", function(req, res) {
+	res.send("Ajax works");
 });
  
-app.post("/user/add", function(req, res) { 
-	/* some server side logic */
-	res.send("OK");
+app.post("/addEvent", function(req, res) { 
+	
+	console.log(JSON.stringify(req));
+	
+	//var tmp = JSON.parse(req);
+	db.save('conferences', 'nodeconf', req);
+	/*db.get('conferences', 'nodeconf',
+		function(err, data) {
+			if (err) console.log("test");
+			else console.log("Found the conference:" +
+				(data.fname));
+		});*/
+	//db.remove('conferences', 'nodeconf');
+	//console.log(req);
+	res.send(JSON.stringify('OK'));
 });
 
 /* serves all the static files */
 app.get(/^(.+)$/, function(req, res){ 
-	console.log('static file request : ' + req.params);
 	res.sendfile( __dirname + req.params[0]); 
 });
  
